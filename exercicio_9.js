40f799f759b12a9e25a18b86a6562efa92869959
tamyrhuana <tamyres.rsmagalhaes@senacsp.edu.br>
2024-08-27 19:55:04 -0300

    exercicio_9_resolvido.js

---
+++ exercicio_9.js
@@ -0,0 +1,42 @@
+let textoMenu = `Escolha o turno em que você estuda:
+                M - Matutino
+                V - Vespertino
+                N - Noturno`;
+
+let turnoescolhido = (prompt(textoMenu));
+let turno;
+let mensangem = "";
+
+switch (turnoescolhido) {
+        case 'M':
+                turno = "Matutino";
+                mensangem ="Bom dia!";
+                break;
+
+        case 'V':
+                turno = "Vespertino";
+                mensangem = "Boa tarde!";
+                break;
+
+        case 'N':
+                turno = "Noturno";
+                mensangem = "Boa noite!";
+                break;
+
+        case "Matutino":
+                turno = "Matutino";
+                mensangem = "Bom dia!";
+                break;
+                
+                case "Vespertino":
+                turno = "Vespertino";
+                mensangem = "Boa tarde!";
+                break;
+                
+        case "Noturno":
+                turno = "Norturno";
+                mensangem = "Boa noite!";
+                break;
+                
+}
+alert(`${mensangem}`);
